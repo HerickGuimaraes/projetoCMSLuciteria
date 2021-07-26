0416905bf5b696932ee4ee388e6ef5c533be49b0
Herick Guimaraes <80368170+SDGuimaraes@users.noreply.github.com>
2021-07-26 02:48:36 -0300

    .

--- resources/views/admin/configuracoes.blade.php
+++ resources/views/admin/configuracoes.blade.php
@@ -34,7 +34,7 @@
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col=sm-2 col-form-label">Sub-Titulo do site</label>
+                    <label class="col=sm-3 col-form-label">Sub-Titulo  </label>
                     <div class="col-sm-10">
                         <input type="text" name="subtitle" value="{{$settings['subtitle']}}" class="form-control">
                     </div>
